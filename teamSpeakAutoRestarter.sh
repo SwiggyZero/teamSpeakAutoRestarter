3954238a7d499c91cfd7024050a2cc91a80deeed
SwiggyZero <38530587+SwiggyZero@users.noreply.github.com>
2020-01-25 20:27:02 -0600

    Add files via upload

---
+++ teamSpeakAutoRestarter.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+#                   In 'teamSpeakDireactory', replace "~/this/is/a/place/holder" with your Team Speak server's direactory.
+#             IN 'teamSpeakPid', replace "~/this/is/a/place/holder" with your Team Speak server's direactory BUT keep "ts3server.pid".
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+
+teamSpeakDireactory=~/this/is/a/place/holder
+teamSpeakPid=~/this/is/a/place/holder/ts3server.pid
+
+if [ -f "$teamSpeakPid" ]
+	then
+		exit
+	else
+		cd "$teamSpeakDireactory"; ./ts3server_startscript.sh start
+  fi
+  
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+#                       Once you have replaced the direactorys, use the command "crontab -e" and paste this command
+#
+#                                    */2 * * * * bash ~/this/is/a/place/holder/teamSpeakAutoRestarter.sh
+#
+#                     NOTE: Make sure to change "~/this/is/a/place/holder" with direactory where this script is located.
+#                Just replace the number(2) if you want to change when Crontab runs the script. It is set to check by minutes. 
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+# 
+#                                                             Author: SwiggyZero
+#                                                            Twitter: @SwiggyZero
+#                                                          Discord: SwiggyZero#00001
+#                                                  GitHub:  https://www.github.com/SwiggyZero
+#
+# ------------------------------------------------------------------------------------------------------------------------------------------------
