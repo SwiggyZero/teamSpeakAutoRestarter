3e1a5521b101c8da89f5d0ad9aef32dc97512a15
SwiggyZero <38530587+SwiggyZero@users.noreply.github.com>
2020-01-25 21:00:36 -0600

    Update teamSpeakAutoRestarter.sh

--- teamSpeakAutoRestarter.sh
+++ teamSpeakAutoRestarter.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------
-#                   In 'teamSpeakDireactory', replace "~/this/is/a/place/holder" with your Team Speak server's direactory.
-#             IN 'teamSpeakPid', replace "~/this/is/a/place/holder" with your Team Speak server's direactory BUT keep "ts3server.pid".
+#                   In 'teamSpeakDirectory', replace "~/this/is/a/place/holder" with your Team Speak server's direactory.
+#             IN 'teamSpeakPid', replace "~/this/is/a/place/holder" with your Team Speak server's directory BUT keep "ts3server.pid".
 # ------------------------------------------------------------------------------------------------------------------------------------------------
 
-teamSpeakDireactory=~/this/is/a/place/holder
+teamSpeakDirectory=~/this/is/a/place/holder
 teamSpeakPid=~/this/is/a/place/holder/ts3server.pid
 
 if [ -f "$teamSpeakPid" ]
   then
     exit
   else
-    cd "$teamSpeakDireactory"; ./ts3server_startscript.sh start
+    cd "$teamSpeakDirectory"; ./ts3server_startscript.sh start
   fi
   
 # ------------------------------------------------------------------------------------------------------------------------------------------------
-#                       Once you have replaced the direactorys, use the command "crontab -e" and paste this command.
+#                       Once you have replaced the directory's, use the command "crontab -e" and paste this command.
 #
 #                                    */2 * * * * bash ~/this/is/a/place/holder/teamSpeakAutoRestarter.sh
 #
-#                     NOTE: Make sure to change "~/this/is/a/place/holder" with direactory where this script is located.
+#                     NOTE: Make sure to change "~/this/is/a/place/holder" with directory where this script is located.
 #                Just replace the number(2) if you want to change when Crontab runs the script. It is set to check by minutes. 
 # ------------------------------------------------------------------------------------------------------------------------------------------------
 
